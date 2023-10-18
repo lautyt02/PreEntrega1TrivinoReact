4812aeeb54804c1ef047e61b81fa6957a3464bc4
LautyT02 <lautyt02@gmail.com>
2023-10-18 16:10:57 -0300

    Added Footer component and worked on style

--- e-comerce/src/App.js
+++ e-comerce/src/App.js
@@ -1,14 +1,18 @@
-import MiNavBar from './componentes/NavBar.js'
-import ItemListContainer from './componentes/ItemListContainer.js'
 import './App.css'
+import MiNavBar from './componentes/NavBar'
+import ItemListContainer from './componentes/ItemListContainer'
+import MiFooter from './componentes/Footer'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <MiNavBar nombre="FHierro's"/>
+        <MiNavBar nombre="FHierro's" />
       </header>
-      <ItemListContainer contenido="Holaaaa"/>
+      <section>
+        <ItemListContainer contenido="Holaaaa" />
+      </section>
+      <MiFooter />
     </div>
   )
 }
