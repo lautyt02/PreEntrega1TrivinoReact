25ffc34a1ef89d7d028ffc9e29ea61422596a8a7
LautyT02 <lautyt02@gmail.com>
2023-10-26 19:55:23 -0300

    Created component for Detailed view

---
+++ e-comerce/src/componentes/ItemDetailContainer.js
@@ -0,0 +1,8 @@
+const ItemDetailContainer=({mensaje})=>{
+    return (
+        <section className="detalle-mi-container">
+            {mensaje}
+        </section>
+    )
+}
+export default ItemDetailContainer
