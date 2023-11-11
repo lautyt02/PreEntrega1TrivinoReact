fdfde468b8e44fdd3469aad4fb5b355c024f06b3
LautyT02 <lautyt02@gmail.com>
2023-11-11 02:41:30 -0300

    Worked on ItemCard widget

--- e-comerce/src/componentes/ItemCard.js
+++ e-comerce/src/componentes/ItemCard.js
@@ -1,8 +1,11 @@
+import { Link } from "react-router-dom"
+
 const ItemCard = ({ titulo }, { img }, { descripcion }) => {
     return (
         <div className="item-card">
             <h2>{titulo}</h2>
             <img src={img} />
+            <Link to={'/item/mi-item'} ><button>Detalle</button></Link>
             <p>
                 {descripcion}
             </p>
